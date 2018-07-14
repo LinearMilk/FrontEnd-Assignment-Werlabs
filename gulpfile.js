a97befc2cada9e212e081ec6d005c97ea8362936
LinearMilk <witold.henszel@gmail.com>
2018-07-14 14:40:20 +0200

    Adding dev server
    using gulp-connect

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,39 @@
-'use strict'
+"use strict";
 
 // Load plugins
-var gulp            = require('gulp');
-var autoprefixer    = require('gulp-autoprefixer');
-var sass            = require('gulp-sass');
-var sourcemaps      = require('gulp-sourcemaps');
+var gulp = require("gulp");
+var autoprefixer = require("gulp-autoprefixer");
+var sass = require("gulp-sass");
+var sourcemaps = require("gulp-sourcemaps");
+var connect = require("gulp-connect");
 
+//dev server
+gulp.task("webserver", function() {
+  connect.server();
+});
 
 // SCSS
-gulp.task('scss', function () {
-    gulp.src('scss/werlabs.scss')
-        .pipe(sourcemaps.init())
+gulp.task("scss", function() {
+  gulp
+    .src("scss/werlabs.scss")
+    .pipe(sourcemaps.init())
 
-        .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
+    .pipe(sass().on("error", sass.logError))
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 versions"],
+        cascade: false
+      })
+    )
 
-        .pipe(sourcemaps.write('./'))
+    .pipe(sourcemaps.write("./"))
 
-        .pipe(gulp.dest('css'))
+    .pipe(gulp.dest("css"));
 });
-
 
 // Watch
-gulp.task('default', function() {
-    gulp.watch(['scss/werlabs.scss'], ['scss']);
+gulp.task("watch", function() {
+  gulp.watch(["scss/werlabs.scss"], ["scss"]);
 });
+
+gulp.task("default", ["scss", "webserver", "watch"]);
